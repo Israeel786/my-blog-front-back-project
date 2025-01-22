8213b7ceeea4a33f218ede2c08ccce9a2847fed0
Israeel786 <smohammadisraeel@gmail.com>
2025-01-22 23:09:57 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const cors = require("cors")
 const app = express();
 app.use(cors(
   {
-    origin:["https://my-blog-front-ruddy.vercel.app"],
+    origin:[""],
     method:["POST","GET"],
     credentials: true
   }
